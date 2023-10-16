df1e7f74a688bb2a2e8748e2abd5a01951ea71dd
Srijita Chandra <srijita.chandra14@gmail.com>
2023-10-16 11:32:03 -0500

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -1,10 +1,25 @@
-import java.util.*;
+//import java.util.*;
 
 public class Statistics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int setSize = 100000;
+        int bitsPerElement = 10;
+        
+		BloomFilterFNV fnvFilter1 = new BloomFilterFNV(setSize, bitsPerElement);
+		BloomFilterFNV fnvFilter2 = new BloomFilterFNV(setSize, bitsPerElement);
+	}
+	
+	public static int estimateSetSize(BloomFilterFNV f) {
+		int elementEstimate = 0;
+		
+		return elementEstimate;
+	}
+	
+	public static int estimateIntersectSize(BloomFilterFNV f1, BloomFilterFNV f2) {
+		int intersectionSize = 0;
+		
+		return intersectionSize;
 	}
 
 }
